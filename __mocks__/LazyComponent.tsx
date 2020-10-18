bbba86fb3720ae28a10048b6802f39bbb86e0dd5
Jakub Biesiada <biesiadajakub@icloud.com>
2020-10-18 18:17:00 +0200

    :white_check_mark: add tests

---
+++ __mocks__/LazyComponent.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LazyComponent = () => {
+  return <p>I am lazy component</p>;
+};
+
+export default LazyComponent;
--- __tests__/withSuspense.test.tsx
+++ __tests__/withSuspense.test.tsx
@@ -1,3 +1,38 @@
+import React, { lazy } from 'react';
+import { render } from '@testing-library/react';
+
+import { SuspenseProvider, withSuspense } from '../src';
+
 describe('withSuspense', () => {
-  it('should', () => {});
+  it('should test HOC with context', async () => {
+    const LazyComponent = lazy(() => import('../__mocks__/LazyComponent'));
+
+    const Component = withSuspense()(() => {
+      return <LazyComponent />;
+    });
+
+    const { getByText, findByText } = render(
+      <SuspenseProvider fallback={<p>Loading...</p>}>
+        <Component />
+      </SuspenseProvider>
+    );
+
+    expect(getByText('Loading...')).toBeInTheDocument();
+
+    expect(await findByText('I am lazy component')).toBeInTheDocument();
+  });
+
+  it('should test HOC without context', async () => {
+    const LazyComponent = lazy(() => import('../__mocks__/LazyComponent'));
+
+    const Component = withSuspense(<p>Loading...</p>)(() => {
+      return <LazyComponent />;
+    });
+
+    const { getByText, findByText } = render(<Component />);
+
+    expect(getByText('Loading...')).toBeInTheDocument();
+
+    expect(await findByText('I am lazy component')).toBeInTheDocument();
+  });
 });
