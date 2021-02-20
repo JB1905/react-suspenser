dc3149a918dfccec3f1af3da19d5c30b6487717b
Jakub Biesiada <biesiadajakub@icloud.com>
2021-02-20 21:03:34 +0100

    :art:  update

--- __tests__/withSuspense.test.tsx
+++ __tests__/withSuspense.test.tsx
@@ -7,7 +7,7 @@ import { SuspenseProvider, withSuspense } from '../src';
 describe('withSuspense', () => {
   it('should test HOC with context', async () => {
     const LazyComponent = lazy(() => import('../__mocks__/LazyComponent'));
-    
+
     const text = 'I am lazy component';
     const Fallback = () => <p>Loading...</p>;
 
@@ -27,7 +27,7 @@ describe('withSuspense', () => {
 
   it('should test HOC without context', async () => {
     const LazyComponent = lazy(() => import('../__mocks__/LazyComponent'));
-    
+
     const text = 'I am another lazy component';
     const Fallback = () => <p>Loading another...</p>;
 
